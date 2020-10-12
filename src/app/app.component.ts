7e16d156a56bacb0752feb715b98716594ff9d1f
kimi <sarahkmcaleer@gmail.com>
2020-10-12 19:52:12 -0400

    Injecting FlickrService into Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { FlickrService } from './services/flickr.service';
 
 //When you register a provider at the component level, you get a new instance of the service with each new instance of that component. At the component level, register a service provider in the providers property of the @Component() metadata.
 
 //A dependency doesn't have to be a service—it could be a function, for example, or a value.
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,5 +14,16 @@ import { FlickrService } from './services/flickr.service';
   providers: [FlickrService]
 })
 export class AppComponent {
+
+constructor(private flickrAPI: FlickrService) {
+
+  console.log(flickrAPI)
+
+
+}
+
+
   title = 'my-app';
+
+
 }
--- src/app/services/flickr.service.ts
+++ src/app/services/flickr.service.ts
@@ -10,16 +10,19 @@ export class FlickrService {
 
 //To define a class as a service in Angular, use the @Injectable() decorator to provide the metadata that allows Angular to inject it into a component as a dependency. Similarly, use the @Injectable() decorator to indicate that a component or other class (such as another service, a pipe, or an NgModule) has a dependency.
 
+data: any;
+
   constructor(private http: HttpClient) {
 
 
    };
 
    getFlickrImages() {
-      this.http.get(https://)
+      this.http.get('https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=APIKEY&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1'
+        ).subscribe((data) => this.data = data);
 
+        console.log(this.data);
    }
-
 
 
 
