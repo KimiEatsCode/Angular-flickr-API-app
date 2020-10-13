a5fd50c77c1f0ab2eaceb7f95eaeca81e7ddecbc
Kimi <sarahkmcaleer@gmail.com>
2020-10-13 15:01:55 -0400

    Update flickr service add interfaces; update search function

--- src/app/services/flickr.service.ts
+++ src/app/services/flickr.service.ts
@@ -5,20 +5,45 @@ import { map } from 'rxjs/operators';
 
 //Ajax is basically is a concept to client-side script that communicates between the server and the client machine to perform any type of operations
 
-@Injectable({
-  providedIn: 'root'
-})
-export class FlickrService {
 
 //To define a class as a service in Angular, use the @Injectable() decorator to provide the metadata that allows Angular to inject it into a component as a dependency. Similarly, use the @Injectable() decorator to indicate that a component or other class (such as another service, a pipe, or an NgModule) has a dependency.
 
+export interface FlickrPhoto {
+  farm: string;
+  id: string;
+  secret: string;
+  server: string;
+  title: string;
+}
+
+export interface FlickrOutput {
+  photos: {
+    photo: FlickrPhoto[];
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class FlickrService {
+
+
 data: any;
+currPage = 1;
+prevKeyword: string;
 
   constructor(private http: HttpClient) {};
 
   search_keyword(keyword: string) {
-
+if(this.prevKeyword === keyword) {
+  this.currPage++;
+} else {
+  this.currPage = 1;
+}
  
+this.prevKeyword = keyword;
 
   const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
 
